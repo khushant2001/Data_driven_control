054101f82e1e586984de2fa2dbfa39239b775e84
Khushant Khurana <70731991+khushant2001@users.noreply.github.com>
2024-04-27 17:59:39 -0400

    Update Sindy_MPC_Mass_spring.m

--- Sindy_MPC_Mass_spring.m
+++ Sindy_MPC_Mass_spring.m
@@ -31,7 +31,7 @@ y(end,:) = [];
 
 n=2;
 Theta = [ones(length(y),1),y,u(1:end-1)',y(:,1).^2,y(:,2).^2,u(1:end-1)'.^2];
-lambda = .01; % lambda is our sparsificat ion knob.
+lambda = .001; % lambda is our sparsificat ion knob.
 Xi = sparsifyDynamics(Theta,y_dot, lambda,n);
 functions = ["1","x","x_dot","u","x^2","x_dot^2","u^2"]';
 heading = ["Basis Functions","Velocity","Acceleration"];
@@ -39,17 +39,55 @@ coefficients = [functions,Xi];
 coefficients = vertcat(heading,coefficients);
 disp(coefficients)
 
-%% Constructing the new differential equations:
+%% Integrate the new differential equations
 x_dot_coeff = Xi(:,1);
 x_ddot_coeff = Xi(:,2);
 
-%{
-%% LQR Controller Design
-Q = [10,0;0,1];
-R = [10];
-k = lqr(A_id,B_id,Q,R);
-%}
+x_sindy = [0];
+x_dot_sindy = [0];
+
+% Numerically integrate the differential equations
+setpoint = 1;
+for i = 1:length(t)
+    error = setpoint - x_sindy(end);
+    u = pid_ctrl(error,dt);
+    x_sindy(i) = x_sindy(end) + dt*(x_dot_coeff'*[1;x_sindy(end);x_dot_sindy(end);u;x_sindy(end)^2;x_dot_sindy(end)^2;u^2]);
+    x_dot_sindy(i) = x_dot_sindy(end) + dt*(x_ddot_coeff'*[1;x_sindy(end);x_dot_sindy(end);u;x_sindy(end)^2;x_dot_sindy(end)^2;u^2]);
+end
+
+figure
+plot(t,x_sindy,LineWidth=3)
+hold on
+plot(t(1:end-1),y(:,1),LineWidth=3)
+legend('Sindy Approximation','Actual Value')
+title("Position vs Time")
+xlabel("Time [sec]")
+ylabel("Position [m]")
+box off 
+grid on
+
+figure
+plot(t,x_dot_sindy,LineWidth=3)
+hold on
+plot(t(1:end-1),y(:,2),LineWidth=3)
+legend('Sindy Approximation','Actual Value')
+title("Velocity vs Time")
+xlabel("Time [sec]")
+ylabel("Velocity [m/sec]")
+box off
+grid on
+
 %% Sindy Function Definitions
+
+global integral;
+integral = 0;  % Initialize integral term
+function u = pid_ctrl(error,dt)
+    global integral;
+    kp = 35;
+    ki = 20;
+    integral = integral + error*dt;
+    u = kp * error + ki * integral;
+end
 
 function Xi = sparsifyDynamics(Theta,dXdt , lambda,n)
     % Compute Sparse regression: sequent ial least squares
